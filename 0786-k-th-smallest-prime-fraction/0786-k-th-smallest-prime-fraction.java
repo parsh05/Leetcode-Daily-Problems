464414a56c6f0e990a143b0f201f0c9b09446917
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-05-10 21:35:15 +0530

    Time: 402 ms (48.03%), Space: 70.6 MB (39.3%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -2,40 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
         int n = arr.length;
 
-        // Initialize the priority queue with all the fractions
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                pq.add(new Pair(arr[i], arr[j]));
+        // Initialize the priority queue with fractions having smallest denominator
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for (int i = 0; i < n - 1; i++) {
+            for(int j = n - 1; j >=n-k && i!=j ;j--){
+                pq.add(new Pair(arr[i],arr[j]));
+                if(pq.size()>k)pq.poll();
             }
         }
 
-        // Remove k - 1 smallest fractions from the priority queue
-        for (int i = 0; i < k - 1; i++) {
-            pq.poll();
-        }
-
+        // Pop k-1 smallest fractions from the priority queue
+        
         // Return the k-th smallest fraction
         Pair result = pq.poll();
         return new int[]{result.num, result.den};
     }
 }
 
-class Pair implements Comparable<Pair> {
-    int num;
-    int den;
+class Pair implements Comparable<Pair>  {
+    int num; // numerator index
+    int den; // denominator index
 
     Pair(int num, int den) {
         this.num = num;
         this.den = den;
     }
-
     @Override
     public int compareTo(Pair p) {
         double fraction1 = (double) this.num / this.den;
         double fraction2 = (double) p.num / p.den;
-        return Double.compare(fraction1, fraction2);
+        return Double.compare(fraction2, fraction1);
     }
 }
