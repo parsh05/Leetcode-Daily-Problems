41aadcfd4284142db159dc16ae2428af313a950d
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-05-24 15:02:50 +0530

    Time: 57 ms (89.69%), Space: 41.1 MB (37.92%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,25 +1,24 @@
 class Solution {
     int maxGold = 0;
 
-    private void collectMaxGold( int[][] grid, int r, int c, int currentVal,  int[][] vis){
-        if( r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || vis[r][c] == 1 || grid[r][c] == 0) return;
+    private void collectMaxGold( int[][] grid, int r, int c, int currentVal){
+        if( r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] <= 0) return;
         currentVal += grid[r][c];
         maxGold = Math.max(currentVal, maxGold );
-        vis[r][c] = 1;
+        grid[r][c] *= -1;
 
-        collectMaxGold(grid, r - 1, c, currentVal, vis);
-        collectMaxGold(grid, r , c + 1, currentVal, vis);
-        collectMaxGold(grid, r , c - 1, currentVal, vis);
-        collectMaxGold(grid, r + 1, c, currentVal, vis);
-        vis[r][c] = 0;
+        collectMaxGold(grid, r - 1, c, currentVal);
+        collectMaxGold(grid, r , c + 1, currentVal);
+        collectMaxGold(grid, r , c - 1, currentVal);
+        collectMaxGold(grid, r + 1, c, currentVal);
+        grid[r][c] *= -1;
     }
 
     public int getMaximumGold(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int[][] vis = new int[m][n];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] != 0 ) collectMaxGold(grid, i, j, 0, vis);
+                if(grid[i][j] != 0 ) collectMaxGold(grid, i, j, 0);
             }
         }
         return maxGold;
