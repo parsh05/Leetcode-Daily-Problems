c42ef0e8778c8bf4b0f2cce6b57362476bcaeb8c
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-06-18 08:49:02 +0530

    Time: 30 ms (32.06%), Space: 45.8 MB (39.31%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -0,0 +1,40 @@
+class Pair implements Comparable<Pair>{
+    int difficulty;
+    int profit;
+
+    Pair(int difficulty, int profit) {
+        this.difficulty = difficulty;
+        this.profit = profit;
+    }
+
+    @Override
+    public int compareTo(Pair o) {
+        if (this.difficulty == o.difficulty)
+            return o.profit - this.profit;
+        return this.difficulty - o.difficulty;
+    }
+}
+
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        Arrays.sort(worker);
+        int n = worker.length;
+        Pair[] diffProfit = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            diffProfit [i] = new Pair( difficulty[i],profit[i] );
+        }
+        Arrays.sort(diffProfit);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> b.profit - a.profit);
+        pq.add(new Pair(-1,0) );
+        int i = 0;
+        int ans = 0;
+        for( int w : worker ){
+            while( i < diffProfit.length && diffProfit[i].difficulty <= w ){
+                pq.add( diffProfit[i]);
+                i++;
+            }
+            ans += pq.peek().profit;
+        }
+        return ans;
+    }
+}
