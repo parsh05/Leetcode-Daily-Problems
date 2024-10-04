8f0abafcfd43a36104def29e8f84dd715bc95449
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-10-04 12:05:08 +0530

    Time: 2 ms (100%), Space: 57.8 MB (25.94%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -1,19 +1,25 @@
 class Solution {
     public long dividePlayers(int[] skill) {
-        Arrays.sort(skill);
         int n = skill.length;
         if(n%2 == 1) return -1;
-        long targetSum = 0;
-        for(int num : skill) targetSum += num;
-        targetSum /= (n/2);
-        int i = 0, j = n-1;
-        long ans = 0;
-        while(i < j){
-            if(skill[i]+ skill[j] != targetSum) return -1;
-             ans += (skill[i] * skill[j]);
-             i++;
-             j--;
+        long sum = 0;
+        for(int num : skill) sum += num;
+        if(sum % (n/2) != 0 ) return -1;
+        long targetSum = sum / (n/2);
+        int[] count_target = new int[(int)targetSum + 1];
+        for(int num : skill){
+            if(num > targetSum ) return -1;
+            count_target[num]++;
         }
-        return ans;
+        long ans = 0;
+        for(int i = 0; i < n; i++){
+            int num = skill[i];
+            
+            if(count_target[(int)(targetSum - num)] > 0){
+                count_target[(int)(targetSum - num)]--;
+                ans += (num * ((int)(targetSum-num)));
+            } else return -1;
+        }
+        return ans/2;
     }
 }
