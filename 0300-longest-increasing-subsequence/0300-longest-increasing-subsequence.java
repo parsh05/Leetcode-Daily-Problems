4e9bfcaf7ff9bfe521bfb7b301d85a9128187c1e
Parshant Kumar Singh <146373184+parsh05@users.noreply.github.com>
2024-10-30 19:34:25 +0530

    Time: 71 ms (23.12%), Space: 44 MB (66.41%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,60 @@
 class Solution {
-    int lis(int[] arr, int i, int prevI, int[][] dp){
-        if(i == arr.length) return 0;
-        if(dp[i][prevI+1] != -1) return dp[i][prevI + 1];
+    int lis(int[] arr, int i, int prevI, int[][] dp) {
+        if (i == arr.length)
+            return 0;
+        if (dp[i][prevI + 1] != -1)
+            return dp[i][prevI + 1];
         int include = 0, exclude = 0;
-        if(prevI == -1 || arr[i] > arr[prevI] ){
-            include = 1 +  lis(arr, i+1, i, dp);
+        if (prevI == -1 || arr[i] > arr[prevI]) {
+            include = 1 + lis(arr, i + 1, i, dp);
             exclude = lis(arr, i + 1, prevI, dp);
-        } else{
+        } else {
             exclude = lis(arr, i + 1, prevI, dp);
         }
 
-        return dp[i][prevI+1] = Math.max(include, exclude);
+        return dp[i][prevI + 1] = Math.max(include, exclude);
     }
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n+1][n+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
-        return lis(nums, 0, -1, dp);
+
+    // public int lengthOfLIS(int[] arr) {
+    //     int n = arr.length;
+    //     int[][] dp = new int[n + 1][n + 1];
+    //     for (int[] row : dp)
+    //         Arrays.fill(row, 0);
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         for (int prevI = i - 1; prevI >= -1; prevI--) {
+    //             int include = 0, exclude = 0;
+    //             if (prevI == -1 || arr[i] > arr[prevI]) {
+    //                 include = 1 + dp[i+1][i+1];
+    //             }
+    //                 exclude = dp[i+1][prevI+1];
+    //             dp[i][prevI + 1] = Math.max(include, exclude);
+    //         }
+    //     }
+    //     return dp[0][-1 + 1];
+    // }
+
+    public int lengthOfLIS(int[] arr) {
+        int n = arr.length;
+        // int[][] dp = new int[n + 1][n + 1];
+        int[] prev = new int[n+1];
+        int[] curr = new int[n+1];
+       
+        for (int i = n - 1; i >= 0; i--) {
+            for (int prevI = i - 1; prevI >= -1; prevI--) {
+
+                int include = 0, exclude = 0;
+                if (prevI == -1 || arr[i] > arr[prevI]) {
+                    include = 1 + prev[i + 1];
+                }
+                    exclude = prev[prevI + 1];
+                curr[prevI + 1] = Math.max(include, exclude);
+                
+            }
+            int[] temp = prev;
+                prev = curr;
+                curr = temp;
+                // Arrays.fill(curr, 0);
+        }
+        return prev[-1 + 1];
     }
 }
